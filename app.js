da288f60b60a8eaa8574865c6b82c2bd0bda4486
Dave Hernandez <brodave318@gmail.com>
2017-06-12 14:56:14 -0700

    Created an NPM start script | Installed Express | Installed node_modules folder | Created dependencies on package.json file

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var aloha = 'Aloha Variables';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = 3000;
+app.listen(port, function(err) {
+  console.log('The server is running on port: ' + port);
+});
