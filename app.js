e35fe7de9c3316d9910996e956fda07f38690539
Dave Hernandez <brodave318@gmail.com>
2017-06-12 15:03:39 -0700

    Routed the app

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 
 var port = 3000;
+
+app.get('/', function(req, res) {
+  res.send('Aloha World!');
+});
+
+app.get('/routing', function(req, res) {
+  res.send('Aloha Routing!');
+});
+
 app.listen(port, function(err) {
   console.log('The server is running on port: ' + port);
 });
