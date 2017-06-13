5dde2ef2ab7e08f72ccc1a5a59d1d3454bdb7dbf
Dave Hernandez <brodave318@gmail.com>
2017-06-13 13:36:05 -0700

    Configured routes | Installed & configured Bower and Bootstrap

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var port = 3000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
 
 app.get('/', function(req, res) {
   res.send('Aloha World!');
