535edaa18955a759f135c6c98077d26a0b3b7275
Dave Hernandez <brodave318@gmail.com>
2017-06-14 14:43:05 -0700

    Installed & configured Wiredep | Injected .js files & .css files automatically to index.html

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Build an "Events" tracking app from the course 'Node.js - From Zero to Web apps"
 * Bower
 * Bootstrap
 * Gulp
+* Jshint
 * Node template engines
 * MongoDB
 
--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,16 @@ gulp.task('style', function() {
   return gulp.src(jsFiles)
     .pipe(jshint());
 });
+
+gulp.task('inject', function() {
+  var wiredep = require('wiredep').stream;
+  var options = {
+    bowerJson: require('./bower.json'),
+    directory: './bower_components',
+    ignorePath: '../../bower_components'
+  }
+
+  return gulp.src('./src/views/*.html')
+    .pipe(wiredep(options))
+    .pipe(gulp.dest('./src/views'));
+});
