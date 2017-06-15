fb11b547366c98dec548f9e485c956ec15154063
Dave Hernandez <brodave318@gmail.com>
2017-06-15 13:36:33 -0700

    Made the nav bar Ejs-compatible

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   //res.send('Aloha World!');
-  res.render('index', { list: ['first val', '2nd val', '3rd val']});
+  res.render('index', {
+    list: ['first val', '2nd val', '3rd val'],
+    nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+  });
 });
 
 app.get('/routing', function(req, res) {
