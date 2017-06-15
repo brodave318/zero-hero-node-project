fea95df5f4127a5a452262d9d0de0f72043530e5
Dave Hernandez <brodave318@gmail.com>
2017-06-15 13:04:39 -0700

    Installed and configured Ejs

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var app = express();
 var port = 3000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send('Aloha World!');
+  //res.send('Aloha World!');
+  res.render('index', { list: ['first val', '2nd val', '3rd val']});
 });
 
 app.get('/routing', function(req, res) {
