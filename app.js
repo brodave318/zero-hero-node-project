c02a05e3db5be14dcd233d0062368a98cc44e67b
Dave Hernandez <brodave318@gmail.com>
2017-06-16 12:32:25 -0700

    Add Events to navbar, routed it and tested it

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 var port = 3000;
+var eventRouter = express.Router();
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
@@ -9,11 +10,29 @@ app.use(express.static('bower_components'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+eventRouter.route('/')
+  .get(function(req, res) {
+    res.send('Hello Events!');
+  })
+
+eventRouter.route('/event')
+  .get(function(req, res) {
+    res.send('Hello Single Event!');
+  })
+
+app.use('/Events', eventRouter)
+
 app.get('/', function(req, res) {
   //res.send('Aloha World!');
   res.render('index', {
     list: ['first val', '2nd val', '3rd val'],
-    nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+    nav: [{Link : 'Services', Text : 'Services'},
+          {Link : 'Portfolio', Text : 'Portfolio'},
+          {Link : 'About', Text : 'About'},
+          {Link : 'Team', Text : 'Team'},
+          {Link : 'Contact', Text : 'Contact'},
+          {Link : 'Events', Text : 'Events'},
+    ]
   });
 });
 
